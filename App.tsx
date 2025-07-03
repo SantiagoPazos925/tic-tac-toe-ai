74e6afdb4ac860ea530d79061341d698f1e06d21
SantiagoPazos925 <santiagopazos925@gmail.com>
2025-07-03 14:33:25 -0300

    Fix TypeScript build errors for Vercel deployment

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { BoardState } from './types';
 import Board from './components/Board';
 import Lobby from './components/Lobby';
